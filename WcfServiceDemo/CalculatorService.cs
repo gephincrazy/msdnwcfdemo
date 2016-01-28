634a2cfec1c9eb53977c7b9b4b99a04947a8ae8e
gephincrazy <gephincrazy@gmail.com>
2016-01-29 00:28:43 +0800

    add wcf demo

---
+++ WcfServiceDemo/CalculatorService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Microsoft.ServiceModel.Samples
+{
+    public class CalculatorService: ICalculator
+    {
+        public double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
+    }
+}
